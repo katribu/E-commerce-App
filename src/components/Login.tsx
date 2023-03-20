af1d3329cdaf10b9cdb90649017cbefab88ef25c
Kat <katrinaburwash_17@hotmail.com>
2023-03-20 15:34:01 +0100

    first commit

---
+++ src/components/Login.tsx
@@ -0,0 +1,15 @@
+
+
+
+export default function Login() {
+    return (
+        <div>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Log In</button>
+
+            <p>email</p>
+            <p>password</p>
+        </div>
+    )
+}
---
+++ src/features/users.ts
@@ -0,0 +1,19 @@
+import { createSlice} from "@reduxjs/toolkit";
+
+
+const initialStateValue: {name: string; email:string;} = { name: "", email: "" };
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: { value: initialStateValue },
+  reducers: {
+    login: (state, action) => {
+      state.value = action.payload;
+    },
+
+  },
+});
+
+export const { login, } = userSlice.actions;
+
+export default userSlice.reducer;
